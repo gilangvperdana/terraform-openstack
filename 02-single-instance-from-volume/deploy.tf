c4020fecac6ff03d80550bd0d5ce074aebd3cf00
gilangvperdana <gilangvirga271@gmail.com>
2022-01-18 03:05:35 +0000

    add req providers version

--- 02-single-instance-from-volume/deploy.tf
+++ 02-single-instance-from-volume/deploy.tf
@@ -1,3 +1,13 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
 resource "openstack_compute_instance_v2" "Instance" {
   name = var.instance_name
   flavor_id = var.flavor_id
--- 05-multiple-instances-from-volume/deploy.tf
+++ 05-multiple-instances-from-volume/deploy.tf
@@ -1,3 +1,13 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
 resource "openstack_compute_instance_v2" "Instance" {
   count = var.instance_num
   name = format("%s-%02d", var.instance_name, count.index+1)
